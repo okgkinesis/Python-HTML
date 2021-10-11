7ef4baab2d9403180dd478039d71efa1c14bdc04
okgkinesis <73905469+okgkinesis@users.noreply.github.com>
2021-10-10 20:36:15 -0400

    Add files via upload

---
+++ project4/assets/js/header.js
@@ -0,0 +1,7 @@
+function header() {
+    document.write("<div style='text-align: center;'>");
+    document.write("<p><a href='index.htm'>Homepage</a>-<a href='chatroom.html'>Chat Room</a>");
+    document.write("-<a href='winners.html'>Winners</a>");
+    document.write("</div>");
+    document.write("</p>");
+}
