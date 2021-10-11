a2438f9bf407f42c8741742616dcb2e173b5cae1
okgkinesis <73905469+okgkinesis@users.noreply.github.com>
2021-10-10 20:38:00 -0400

    Delete project4 directory

--- project4/assets/js/header.js
+++
@@ -1,7 +0,0 @@
-function header() {
-    document.write("<div style='text-align: center;'>");
-    document.write("<p><a href='index.htm'>Homepage</a>-<a href='chatroom.html'>Chat Room</a>");
-    document.write("-<a href='winners.html'>Winners</a>");
-    document.write("</div>");
-    document.write("</p>");
-}
